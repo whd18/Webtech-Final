782e6e4a005d1f5a54e505ba82594b194c648d7d
Rakibul Hasan <rakib.rumi09@gmail.com>
2016-04-11 00:48:50 +0600

    uploading some documents t
    our competed tasks

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tourism</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='https://fonts.googleapis.com/css?family=Candal' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,600,800' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+	<?php
+		require_once('nav.php');
+	?>
+	
+	<div id="slider">
+		<p>LET US <br />SHOW YOU <br /> <b>BANGLADESH</b></p>
+		<img src="img/slider.jpg">
+	</div>
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tourism</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Candal' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,600,800' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+	<Header >
+		<a href="#" id="logo">
+			<img src="img/logo.jpg">
+		</a>
+		<nav>
+		    <ul>
+		    	<!-- <li><a href="#"><img src="img/bnc_logo.gif"></a></li> -->
+		    	<li><a href="#"><span>Destination</span></a></li>
+		        <li><a href="#"><span>Recomended Places</span></a></li>
+		        <li><a href="#"><span>Route </span></a></li>
+		        <li><a href="#"><span>Package</span></a></li>             
+		        <li><a href="#"><span>Sign In </span></a></li>
+		        <li><a href="#"><span>Contact Us</span></a></li>
+		    </ul>
+		</nav>
+
+	</Header>
