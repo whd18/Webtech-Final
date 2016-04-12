c44565d99d24b33f11f2238ce199dcf2871095d2
Md. Wahid Rahman <tech.whd@gmail.com>
2016-04-13 01:08:02 +0600

    Added files via upload

---
+++ coxs.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<?php
+	require_once('nav.php');
+?>
+<html>
+<head>
+	<title> Cox's Bazar</title>
+	<br/><br/>
+	<h1 style="text-align: center;">Visit Cox's Bazar</h1>
+	<style>
+		#place {
+		   float: left;
+		   margin: 10px;
+		   padding: 10px;
+		   width: auto;
+		   height: auto;
+		   border: 3px ridge  #a6a6a6;
+		   font-size: 20px;
+		} 
+	</style>
+</head>
+<body>
+<div id="place">
+
+			<h2 style="text-align: center;">Himchori Waterfall</h2>
+			<img src="img/himchori.jpg" height="200" width="auto" alt="cox's bazar" align="left" style="margin: 5px 10px 5px 10px;">
+			<img src="img/himchori2.jpg" height="200" width="auto" alt="cox's bazar" align="right" style="margin: 5px 5px 5px 5px;">
+				<P>Many attractions are for the tourists around Cox's Bazar. You can go Himchori to see it's Waterfall, this is famous for waterfall, though in the winter season it dwindles but during the rainy season it's really a fabulous and full waterfall can be enjoyed.
+				</P>
+				<p>The amazing waterfall of Himchari is a very rare scene to enjoy. The waterfall from the green hill is relatively extraordinary to look at. However, in the winter it dwindles whereas in the rainy season it is really wonderful and full waterfall could be enjoyed. The spot is ideal for picnic, shooting, relaxing and sunbathing. But whenever you have sunbathing locals can rush around you as Bangladeshis' are not used to see women in swim costume. Here you get beautiful hilltop resort centre where you can stay for relaxing and can hear the shore of the Bay of Bengal.</p>
+				<p>Himchari National Park: 
+				Park is a beautiful tropical rain forest (evergreen and semi-ever-green) around the South Asia. The park was established in 1980. This place is maintained, protected and secured by the government for the foreign tourists and the local visitors. The Park is not only attractive for the tourists it is also attractive for the biologist. 
+				The park gets high forest, low forest and grasslands with a few trees. Around 58 species of trees, 4 species grasses, 15 species of shrubs, 19 climbers and 21 species of herbs altogether 117 plant species are available here. In the forest area there are at least twenty kinds of important herbs like Curculigorecurvata, Swintonia floribunda and Alpinianigra are available which most valuable plant are for the biologist. 
+				</p>
+				<p><b>Note: </b>You can reach Himchari only by the beach. So, ensure you are back to town before high tide rise in the evening. It is not wise to stay here after sunset.</p>
+
+	</div>
+
+<div id="place">
+
+			<h2 style="text-align: center;">Inani Beach</h2>
+			<img src="img/inani2.jpg" height="200" width="auto" alt="cox's bazar" align="left" style="margin: 5px 10px 5px 10px;">
+			<img src="img/inani.jpg" height="200" width="auto" alt="cox's bazar" align="right" style="margin: 5px 5px 5px 5px;">
+			<p>Inani Beach is within Ukhia Thana, 35 km. to the south of Cox's Bazar.With green hills to the east, the golden beach of Inani casts a music spell on anyone stepping on to its fine golden sands. The clean blue waters of the Bay are ideal for swimming. 
+			It is considered the world's longest and broadest beach: 180m at high tide and 300m at low tide.</p>
+			<p>To get here, take a bus to Teknaf and get off at Court Bazar (30km), a tiny village 2km before Ukhia. From there, you can rickshaw or tempo west to the beach, 10 km away. If you're headed for the guesthouse, ask the rickshaw driver to let you off at the tiny village of Sonarpara. From there, walk south until you reach the guesthouse. Alternatively, hire a jeep from Cox's Bazar to take you directly to Inani using the scenic beach route. </p>
+			<p><b>Note: </b>Every year numerous foreign and local tourists come here to spend their leisure in Inani Beach. However, it lacks several amenities that visitors can expect. It is wise to take a packed lunch and water with you when you visit the beach. But those people who have taken the time to explore the beach can testify that its beauty and cool breeze far outweighs anything it might be lacking in amenities. When visiting Inani Beach, ensure you have camera along to capture the amazing sight of the sun setting over the sea. </p>
+
+
+	</div>
+
+</body>
+</html>
