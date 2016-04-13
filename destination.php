a2ffb06148a05ec2426bfb247c4d929eba4ae1bc
whd18 <tech.whd@gmail.com>
2016-04-14 05:24:41 +0600

    Destination
    Totally modified.

--- destination.php
+++ destination.php
@@ -3,78 +3,33 @@
 	require_once('nav.php');
 ?>
 <html  style="background-color:#D2D2D2">
-	<head>
-		<style>
-			#div1 {
-			    height:10%;
-			    width: 50%;
-			    float: left;
-			    color:#00004d;
-			    font: 20px Arial, Helvetica;
-			    text-align: justify;
-			}
-			#div2 {
-			    height:10%;
-			    width:50%;
-			    float: right;
-			    text-align: left;
-			    color:#00004d;
-			    font: 20px Arial, Helvetica;
-			    text-align: justify;
-			}
+<head>
+	<title> Destination</title>
+	<br/><br/><br/>
+	<style>
+		.place {
+		   float: left;
+		   margin: 10px;
+		   padding: 10px;
+		   width: 650px;
+		   height: auto;
+		   color:#00004d;
+		   background-color: #F4FAFF;
+		   border: 3px ridge  #a6a6a6;
+		   text-align: justify;font:bold 20px Arial, Helvetica;
+		} 
 
-			#div3 {
-			    height:10%;
-			    width: 50%;
-			    float: right;
-			    color:#00004d;
-			    font: 20px Arial, Helvetica;
-			    text-align: justify;
-			}
-			#div4 {
-			    height:10%;
-			    width: 50%;
-			    float: left;
-			    color:#00004d;
-			    font: 20px Arial, Helvetica;
-			    text-align: justify;
-			}
-			#div5 {
-			    height:10%;
-			    width: 50%;
-			    float: left;
-			    color:#00004d;
-			    font: 20px Arial, Helvetica;
-			    text-align: justify;
-			}
-			#div6 {
-			    height:10%;
-			    width: 50%;
-			    float: left;
-			    color:#00004d;
-			    font: 20px Arial, Helvetica;
-			    text-align: justify;
-			}
-			#footer {
-			     text-shadow: white;
-			    color:black;
-			    text-align:center;	 
-			}
-		</style>
-
-
-	</head>
-
+	</style>
+</head>
 <body>
-
-	<div id="div1" style="margin: 5px,5px,5px,5px;">
+	<div class="place">
 		
 		<br/><br/>
 		<h1 style="text-align: center;">Cox's Bazar</h1>
 			<label><b>Chittagong , Bangladesh.</b></label>
 			<img src="img/cox.jpg" height="200" width="300" alt="cox's bazar" align="left" style="margin: 10px 10px 10px 10px;">
 				<P style="margin: 10px 10px 10px 10px; ">Cox's Bazar is a town, a fishing port and district headquarters in Bangladesh. The beach in Cox's Bazar is an unbroken 125 km sandy sea beach with a gentle slope, one of the world's longest.<br/><br/> It is located 150 km south of the industrial port Chittagong. Cox's Bazar is also known by the name Panowa, whose literal translation means "yellow flower." Its other old name was "Palongkee".
-				The modern Cox's Bazar derives its name from Captain Hiram Cox (died 1799), an officer of the British East India Company.Cox was appointed Superintendent of Palongkee outpost after Warren Hastings became Governor of Bengal. 
+				The modern Cox's Bazar derives its name from Captain Hiram Cox (died 1799), an officer of the British East India Company.Cox was appointed Superintendent of Palongkee 
 					<div id="footer">
 						<p><a href="coxs.php">Details</a></p>
 					</div>
@@ -84,13 +39,13 @@
 	</div>
 
 
-	<div id="div2" style="margin: 5px,5px,5px,5px;">
+	<div class="place">
 		<br/><br/>
 		<h1 style="text-align: center;">Sundarban</h1>
 		  <label><b>Khulna , Bangladesh.</b></label>
 		<img src="img/sundarban.jpg" height="200" width="300" alt="cox's bazar" align="left" style=" margin: 10px 10px 10px 10px;">
 			<p style="margin: 10px 10px 10px 10px; ">The Sundarbans is a natural region comprising southern Bangladesh and a small part in Eastern India. It is the largest single block of tidal halophytic mangrove forest in the world.The Sundarbans covers approximately 10,000 square kilometres (3,900 sq mi) most of which is in Bangladesh with the remainder in India. The Sundarbans is a UNESCO World Heritage Site.<br/><br/>
-			Sundarbans South, East and West are three protected forests in Bangladesh. This region is densely covered by mangrove forests, and is the largest reserves for the Bengal tiger. 
+			Sundarbans South, East and West are three protected forests in Bangladesh. This region is densely covered by mangrove forests. 
 					<div id="footer">
 						<p><a href="sundarban.php">Details</a></p>
 
@@ -99,13 +54,13 @@
 	</div>
 
 
-	<div id="div3" style="margin: 5px,5px,5px,5px;">
+	<div class="place">
 	<h1 style="text-align: center;">Sylhet</h1>
 		<label><b>Sylhet , Bangladesh.</b></label>
 		<img src="img/sylhet.jpg" height="200" width="auto" alt="cox's bazar" align="left" style=" margin: 10px 10px 10px 10px;">
 			<p style="margin: 10px 10px 10px 10px; ">
 				Sylhet is a major city that lies on the banks of Surma River in north-east Bangladesh. It is surrounded by tea estates, sub-tropical hills, rain forests and river valleys; the region is one of the leading tourist destinations in the country. The city has one of the highest literacy rates in the country.Many Sylhetis live abroad, particularly in London of the UK, where they form a prominent community.<br/><br/>
-				Sylhet is a prominent Islamic spiritual centre and home to numerous Sufi shrines. Sylhet has a reputation as the Spiritual Capital of Bangladesh. It hosts the 14th century mausoleums of Shah Jalal and Shah Paran. The Sylhet municipality was constituted during the British Raj in 1867. It was part of the Bengal Presidency and the Assam Province. 
+				Sylhet is a prominent Islamic spiritual centre and home to numerous Sufi shrines. Sylhet has a reputation as the Spiritual Capital of Bangladesh. It hosts the 14th century mausoleums of Shah Jalal and Shah Paran.
 					<div id="footer">
 						<p><a href="#">Details</a></p>
 
@@ -113,7 +68,7 @@
 			</p>
 	</div>
 
-	<div id="div4" style="margin: 5px,5px,5px,5px;">
+	<div class="place">
 	<h1 style="text-align: center;">Bandarban</h1>
 		<label><b>Chittagong , Bangladesh.</b></label>
 		<img src="img/bandarban.jpg" height="200" width="auto" alt="cox's bazar" align="left" style=" margin: 10px 10px 10px 10px;">
@@ -127,12 +82,12 @@
 			</p>
 	</div>
 
-	<div id="div5" style="margin: 5px,5px,5px,5px;">
+	<div class="place">
 	<h1 style="text-align: center;">Kuakata</h1>
 		<label><b>Barishal , Bangladesh.</b></label>
 		<img src="img/kuakata.jpg" height="200" width="300" alt="cox's bazar" align="left" style=" margin: 10px 10px 10px 10px;">
 			<p style="margin: 10px 10px 10px 10px; ">
-				Kuakata is a town known for its panoramic sea beach. It is located in south-eastern Bangladesh, and is the number two tourist destination in the country. Kuakata beach is a sandy expanse 18 kilometres (11 mi) long and 3 kilometres (1.9 mi) wide.[3] From the beach one can have an unobstructed view of both sunrise and sunset over the Bay of Bengal.<br/><br/>In Kuaka You can visit Buddhist Temple where you can see the statue of Goutom Buddha and two wells of 200 years old. Local name of the well is Kua and Kata is a local name of digging a well; so was the name Kuakata. <br/><br/>Fisherman village is another place where you can visit and watch the lifestyle of the Fisherman. If you are adventurous you may also go for fishing on the fishing boat if you can manage the local fishermen. That will give you pleasure and experience, which you won?t be able to gather from anywhere else.
+				Kuakata is a town known for its panoramic sea beach. It is located in south-eastern Bangladesh, and is the number two tourist destination in the country. Kuakata beach is a sandy expanse 18 kilometres (11 mi) long and 3 kilometres (1.9 mi) wide.[3] From the beach one can have an unobstructed view of both sunrise and sunset over the Bay of Bengal.<br/><br/>In Kuaka You can visit Buddhist Temple where you can see the statue of Goutom Buddha and two wells of 200 years old. Local name of the well is Kua and Kata is a local name of digging a well; so was the name Kuakata.Fisherman village is another place where you can visit and watch the lifestyle of the Fisherman. If you are adventurous you may also go for fishing on the fishing boat if you can manage the local fishermen. That will give you pleasure and experience.
 					<div id="footer">
 						<p><a href="#">Details</a></p>
 
@@ -141,7 +96,7 @@
 			</p>
 	</div>
 
-	<div id="div6" style="margin: 5px,5px,5px,5px;">
+	<div class="place">
 	<h1 style="text-align: center;">St. Martin's Island </h1>
 		<label><b>Teknaf , Bangladesh.</b></label>
 		<img src="img/saintmartin.jpg" height="200" width="300" alt="cox's bazar" align="left" style=" margin: 10px 10px 10px 10px;">
@@ -155,5 +110,5 @@
 			</p>
 	</div>
 
-	</body>
+</body>
 </html>
