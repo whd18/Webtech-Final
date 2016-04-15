8403bbfbbad307da73305ba03b7890f2ea889ffa
whd18 <tech.whd@gmail.com>
2016-04-16 03:24:07 +0600

    Destination
    Complete

--- destination.php
+++ destination.php
@@ -62,7 +62,7 @@
 				Sylhet is a major city that lies on the banks of Surma River in north-east Bangladesh. It is surrounded by tea estates, sub-tropical hills, rain forests and river valleys; the region is one of the leading tourist destinations in the country. The city has one of the highest literacy rates in the country.Many Sylhetis live abroad, particularly in London of the UK, where they form a prominent community.<br/><br/>
 				Sylhet is a prominent Islamic spiritual centre and home to numerous Sufi shrines. Sylhet has a reputation as the Spiritual Capital of Bangladesh. It hosts the 14th century mausoleums of Shah Jalal and Shah Paran.
 					<div id="footer">
-						<p><a href="#">Details</a></p>
+						<p><a href="sylhet.php">Details</a></p>
 
 					</div>
 			</p>
@@ -75,7 +75,7 @@
 			<p style="margin: 10px 10px 10px 10px; ">
 				Bandarban is a district in South-Eastern Bangladesh, and a part of the Chittagong Division. It is one of the three districts that make up the Chittagong Hill Tracts, the others being Rangamati District and Khagrachhari District.Bandarban is regarded as one of the most attractive travel destinations in Bangladesh.<br/><br/> Bandarban (meaning the dam of monkeys), or in Marma or Arakanese language as "Rwa-daw Mro" is also known as Arvumi or the Bohmong Circle (of the rest of the three hill districts Rangamati is the Chakma Circle, Raja Devasish Roy and Khagrachari is the Mong Circle, Raja Sachingprue Marma).
 					<div id="footer">
-						<p><a href="#">Details</a></p>
+						<p><a href="bandarban.php">Details</a></p>
 
 					</div>
 
@@ -89,7 +89,7 @@
 			<p style="margin: 10px 10px 10px 10px; ">
 				Kuakata is a town known for its panoramic sea beach. It is located in south-eastern Bangladesh, and is the number two tourist destination in the country. Kuakata beach is a sandy expanse 18 kilometres (11 mi) long and 3 kilometres (1.9 mi) wide.[3] From the beach one can have an unobstructed view of both sunrise and sunset over the Bay of Bengal.<br/><br/>In Kuaka You can visit Buddhist Temple where you can see the statue of Goutom Buddha and two wells of 200 years old. Local name of the well is Kua and Kata is a local name of digging a well; so was the name Kuakata.Fisherman village is another place where you can visit and watch the lifestyle of the Fisherman. If you are adventurous you may also go for fishing on the fishing boat if you can manage the local fishermen. That will give you pleasure and experience.
 					<div id="footer">
-						<p><a href="#">Details</a></p>
+						<p><a href="kuakata.php">Details</a></p>
 
 					</div>
 
@@ -103,7 +103,7 @@
 			<p style="margin: 10px 10px 10px 10px; ">
 				St. Martin's Island is a small island (area only 8 km2) in the northeastern part of the Bay of Bengal, about 9 km south of the tip of the Cox's Bazar-Teknaf peninsula, and forming the southernmost part of Bangladesh. There is a small adjoining island that is separated at high tide, called Chera Dwip.<br/><br/> It is about 8 kilometres (5 miles) west of the northwest coast of Myanmar, at the mouth of the Naf River. The first settlement started just 250 years ago by some Arabian sailors who named the island 'Zajira'.<br/><br/> During British occupation the island was named St. Martin Island. During the First Anglo-Burmese War between the British and Burmese empires in 1824–1826, rival claims to the island were a major factor. The local names of the island are "Narical Gingira".
 					<div id="footer">
-						<p><a href="#">Details</a></p>
+						<p><a href="stmartin.php">Details</a></p>
 
 					</div>
 
